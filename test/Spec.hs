44e6edffd6d1cbb77039a5306ec2ea0128ddf084
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2020-04-19 20:44:19 +0900

    Add unittest

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+
+module Main (main) where
+
+import           Lib                 (app)
+import           Test.Hspec
+import           Test.Hspec.Wai
+import           Test.Hspec.Wai.JSON
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+spec :: Spec
+spec = with (return app) $ do
+  describe "GET /users" $ do
+    it "responds with 200" $ do
+      get "/users" `shouldRespondWith` 200
