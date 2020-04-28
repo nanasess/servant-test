e1f71dda624a53451056efbd8eae5388acc282a7
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2020-04-28 09:38:01 +0900

    example comment out

--- src/Entity/Country.hs
+++ src/Entity/Country.hs
@@ -9,14 +9,30 @@ module Entity.Country where
 import           Database.HDBC.Query.TH          (defineTableFromDB)
 import           Database.HDBC.Record.Query      (runQuery')
 import           Database.HDBC.Schema.PostgreSQL (driverPostgreSQL)
+-- -- import           Database.Relational.Query
 import           Database.Relational.Query.Type  (relationalQuery)
 import           DataSource                      (connect, defineTable)
 import           GHC.Generics                    (Generic)
 
-$(defineTableFromDB connect driverPostgreSQL  "public" "mtb_country" [''Show, ''Generic])
+$(defineTableFromDB
+  connect
+  driverPostgreSQL
+  "public"
+  "mtb_country"
+  [''Show, ''Generic])
 
 showAllCountries :: IO()
 showAllCountries = do
   conn <- connect
   countries <- runQuery' conn (relationalQuery mtbCountry) ()
   mapM_ print countries
+
+main :: IO ()
+main = print "app"
+
+-- showJapan :: Relation () String
+-- showJapan = relation $ do
+--   i <- query mtbCountry
+--   wheres $ i ! mtbCountry.id .=. value 392
+--   return (i ! mtbCountry.name)
+
